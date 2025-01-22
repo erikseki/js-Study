c39a3e932b790feae2535a6f1d0169983051b140
erikseki <erikseki@hotmail.com>
2025-01-22 11:22:08 -0300

    novos arquivos function

---
+++ Function/function.js
@@ -0,0 +1,28 @@
+// parâmetros/Argumentos
+// Retorno
+
+// function exibeNomeEstudante(nome){
+//     console.log(nome);
+// }
+// exibeNomeEstudante('Caroline');
+// exibeNomeEstudante('Ana');
+
+
+
+// function exibeInfoEstudante(nome, nota){
+//     console.log(`o nome é ${nome} e a nota é ${nota}`);
+// }
+// exibeInfoEstudante('Caroline', 10);
+// exibeInfoEstudante('Ana', 7);
+
+
+// usando o return, precisamos colocar o console.log nas chamadas das funções
+// function exibeInfoEstudante(nome, nota){
+//     return `o nome é ${nome} e a nota é ${nota}`;
+// }
+
+
+// console.log(exibeInfoEstudante('Caroline', 10));
+// console.log(exibeInfoEstudante('Ana', 7));
+
+
