d71cde6a155154c51a38622019958b4392fa23df
erikseki <erikseki@hotmail.com>
2025-02-13 10:23:52 -0300

    finalizando os exercicios de modificações do JSON com object e array

--- objetos-arrays/exercicios/exercicio6.js
+++ objetos-arrays/exercicios/exercicio6.js
@@ -16,12 +16,13 @@ const novoAnimal1 = {
   "habitat": "nãosei"
 };
 
+// // exercicio 1 - ler arquivo json
 const dado = require('../json/animais.json')
-
 console.log(dado);
 
 // const novaBiblioteca = JSON.stringify(dado);
 
+// // exercicio 2 - transformar em objeto com .parse
 // const novaBibliotecaToObj = JSON.parse(novaBiblioteca);
 // console.log(novaBibliotecaToObj);
 // console.log(typeof (novaBibliotecaToObj));
@@ -29,7 +30,37 @@ console.log(dado);
 // console.log(dado);
 // console.log(typeof (dado));
 
-dado.animais.push(novoAnimal1);
-console.log(dado);
+// // exercicio 3 - adicionando um novo array com outro animal
+// dado.animais.push(novoAnimal1);
+// console.log(dado);
 
+// // exercicio 4 - modificar uma chave de um array com o forEach ou map
+// dado.animais.forEach(animais =>{
+//   if (animais.id === 1 ){
+//     animais.habitat = 'Pântano';
+//   }
+// })
+
+// // diminuíndo todas as letras do valores da chave Habitat
+// dado.animais = dado.animais.map(animais => {
+//   return {
+//     ...animais,
+//     habitat: animais.habitat.toLowerCase()
+//   }
+// });
+
+
+
+// // exercicio 5 - deletando um array inteiro com o filter 
+// dado.animais = dado.animais.filter(animais => animais.id !== 4);
+
+// // exercicio 6 - alterar o objeto para uma string 
+// const toString = JSON.stringify(dado);
+// console.log(typeof(toString));
+
+// // exercicio 7 - imprima no console o objeto JavaScript resultante
+// console.log(toString);
+
+
+// para atualizar o arquivo JSON.
 fs.writeFileSync('../json/animais.json', JSON.stringify(dado, null, 2), 'utf-8');
